0cac4a92c7d9b368b09f4b93a69ed7a08c0e6436
gautameet <145467217+gautameet@users.noreply.github.com>
2024-01-31 20:24:07 +0100

    Update dash_credit.py

--- dash_credit.py
+++ dash_credit.py
@@ -14,7 +14,6 @@ from zipfile import ZipFile, BadZipFile
 import pickle
 #import shap
 
-
 # Features
 feat = ['SK_ID_CURR','TARGET','DAYS_BIRTH','NAME_FAMILY_STATUS','CNT_CHILDREN',
         'DAYS_EMPLOYED','NAME_INCOME_TYPE','AMT_INCOME_TOTAL','AMT_CREDIT','AMT_ANNUITY']
