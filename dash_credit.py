c036273a6bb37edd06fc3947af0b8a50ebef83dc
gautameet <145467217+gautameet@users.noreply.github.com>
2024-01-31 19:50:47 +0100

    Update dash_credit.py

--- dash_credit.py
+++ dash_credit.py
@@ -2,9 +2,9 @@ import math
 import streamlit as st
 import altair as alt              # for data visualtization
 #import sklearn
-from sklearn.preprocessing import StandardScaler
-from sklearn.neighbors import NearestNeighbors
-from sklearn.neighbors import KNeighborsClassifier
+#from sklearn.preprocessing import StandardScaler
+#from sklearn.neighbors import NearestNeighbors
+#from sklearn.neighbors import KNeighborsClassifier
 from PIL import Image
 import requests
 import plotly
