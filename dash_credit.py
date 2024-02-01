b32ac6ea2035e373c8008468e4a5986e3967289d
gautameet <145467217+gautameet@users.noreply.github.com>
2024-02-01 01:16:57 +0100

    Update dash_credit.py

--- dash_credit.py
+++ dash_credit.py
@@ -16,6 +16,7 @@ import sys
 import shap
 
 
+
 # Features
 feat = ['SK_ID_CURR','TARGET','DAYS_BIRTH','NAME_FAMILY_STATUS','CNT_CHILDREN',
         'DAYS_EMPLOYED','NAME_INCOME_TYPE','AMT_INCOME_TOTAL','AMT_CREDIT','AMT_ANNUITY']
